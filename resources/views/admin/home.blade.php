e38c21fc1ef2a99fcd886b24276a42632b7f5aa2
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-09-02 15:02:51 +0200

    add admin home controller

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>dashboard private</h1>
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>public page</h1>
+@endsection
