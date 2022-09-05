842376dbaf69b4f0af7c087bfcce00184b511fd6
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-09-05 17:34:18 +0200

    add create post validation and function-getslug

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -31,7 +32,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -42,7 +43,17 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->getValidate());
+        $form_data = $request->all();
+
+        $new_post = new Post();
+        $new_post->fill($form_data);
+
+        $new_post->slug = $this->getSlug($new_post->title);
+
+        $new_post->save();
+
+        return redirect()->route('admin.posts.show', ['post' => $new_post->id]);
     }
 
     /**
@@ -92,5 +103,34 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    protected function getSlug($title)
+    {
+        //assegnare lo slug
+        $slug_to_save = Str::slug($title, '-');
+        $slug_base = $slug_to_save;
+
+        //verifico se presente nel db
+        $existing_slug_post = Post::where('slug', '=', $slug_to_save)->first();
+
+        //aggiungo un counter fino a quando non trovo un slug libero
+        $counter = 1;
+        while ($existing_slug_post) {
+            $slug_to_save = $slug_base . '-' . $counter;
+
+            //controllo se presente nel db
+            $existing_slug_post = Post::where('slug', '=', $slug_to_save)->first();
+            $counter++;
+        }
+        return $slug_to_save;
+    }
+
+    protected function getValidate()
+    {
+        return [
+            'title' => 'required|max:255',
+            'content' => 'required|max:60000'
+        ];
     }
 }
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Create a new Post</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action=" {{ route('admin.posts.store') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <textarea class="form-control" id="content" name="content" rows="6">{{ old('content') }}</textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Save Post">
+    </form>
+@endsection
